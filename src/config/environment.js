7a92d16d4f30c7651348f7576521b8f62acff464
mrarvind90 <arvindrankalidas@gmail.com>
2023-09-22 19:54:56 +0800

    Removed .gitattribute and environment.js files since we will move towards Node 20.x version which will support passing of .env files

--- src/config/environment.js
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-
-// TODO: Extend this environment configuration to support spinning up the application on multiple environments
-const env = {
-	PORT: process.env.PORT || 3000,
-	MONGODB_ATLAS_URI: process.env.MONGODB_ATLAS_URI,
-};
-
-export default env;
